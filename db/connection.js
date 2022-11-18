c92b11d4d5151c4692ce0c431eb9c5da509580ff
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-11-18 13:32:55 -0800

    added database to connection.js

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+
+// Database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log("Connected to employee_tracker database.")
+);
+
+
+module.exports = db;
