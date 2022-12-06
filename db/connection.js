1a4729f6fe20a65a6516c3311710660e533d1227
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-12-06 00:04:31 -0800

    fixed format

--- db/connection.js
+++ db/connection.js
@@ -6,9 +6,6 @@ require("dotenv").config();
 const db = mysql.createConnection(
   {
     host: "localhost",
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
     user: "root",
     database: "employee_tracker",
     password:"",
